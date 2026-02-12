20c0dc620e0ddf4113185b6fc9a5b5654052ec99
bilza2023 <bilza2023@gmail.com>
2026-02-12 15:30:40 +0500

    built

--- src/lib/data/courses.static.js
+++ src/lib/data/courses.static.js
@@ -2,9 +2,16 @@ export const courses = [
     {
       id: 101,
       tcode: 'intro-algebra',
-      title: 'Intro to Algebra',
-      thumbnail: '/images/box.webp',
+      title: 'Pre Algebra',
+      thumbnail: '/images/nestedTriangles.webp',
       href: '/syllabus/pre-algebra'
+    },
+    {
+      id: 102,
+      tcode: 'intro-algebra',
+      title: 'Pre Physics',
+      thumbnail: '/images/fbise9physicsChapter1Thumbnail.webp',
+      href: '/syllabus/pre-physics'
     }
   ];
   
