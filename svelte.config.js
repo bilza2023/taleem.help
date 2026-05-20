8fbbb083a49b03ef33a139fea6d55a18918df55a
bilza2023 <bilza2023@gmail.com>
2026-05-20 20:51:54 +0500

    adopter-node

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter
+	from "@sveltejs/adapter-node";
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	compilerOptions: {
-		// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
-		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
-	},
+import { vitePreprocess }
+	from "@sveltejs/vite-plugin-svelte";
+
+export default {
+
+	preprocess:
+		vitePreprocess(),
+
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+
+		adapter:
+			adapter()
 	}
 };
-
-export default config;
