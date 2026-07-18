68208297230317cd83fdadcf37b0c07dfc989dba
bilza2023 <bilza2023@gmail.com>
2026-07-18 22:05:21 +0500

    articles page working

--- src/routes/articles/+page.svelte
+++ src/routes/articles/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+import "@picocss/pico/css/pico.classless.min.css";
 	import { onMount } from "svelte";
 	import { page } from "$app/state";
 
@@ -9,7 +10,6 @@
 	let loading = $state(true);
 
 	onMount(async () => {
-
 		const articleId = page.url.searchParams.get("article");
 
 		if (!articleId) {
@@ -19,7 +19,6 @@
 		}
 
 		try {
-
 			const res = await fetch(`${SERVER}/article/${articleId}`);
 
 			if (!res.ok) {
@@ -27,34 +26,51 @@
 			}
 
 			article = await res.text();
-
-			console.log(article);
-
 		} catch (err) {
-
 			console.error(err);
 			error = err.message;
-
 		} finally {
-
 			loading = false;
-
 		}
-
 	});
 </script>
+
+<style>
+	.container {
+		padding: 0px;
+		margin: 10px 50px;
+		font-size: 1.4rem;
+		line-height: 2.4rem;
+	}
+:global(.container img) {
+    display: block;
+    width: 60%;
+    max-width: 60%;
+    height: auto;
+    margin: 1rem auto;
+}
+:global(.container ul) {
+   margin-left:40px;
+}
+</style>
 {#if loading}
 
-	<p>Loading article...</p>
+<main class="container">
+	<p>Loading article…</p>
+</main>
 
 {:else if error}
 
+<main class="container">
 	<h2>{error}</h2>
+</main>
 
 {:else}
 
-	<h1>{article.title}</h1>
-{@html article}
-	<!-- <pre>{JSON.stringify(article, null, 2)}</pre> -->
+<main class="container">
+	<!-- <article> -->
+		{@html article}
+	<!-- </article> -->
+</main>
 
 {/if}
